434e226d966a44098a9f5e88ae2d5d4fb3c999fb
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-12 14:44:27 +0800

    add API 101 module entrypoint

---
+++ src/main/java/com/hellovoid/liquiddock/ModuleMain.java
@@ -0,0 +1,28 @@
+package com.hellovoid.liquiddock;
+
+import androidx.annotation.NonNull;
+
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import io.github.libxposed.api.XposedModule;
+
+/** libxposed API 101 entry point. */
+public final class ModuleMain extends XposedModule {
+    @Override
+    public void onModuleLoaded(@NonNull ModuleLoadedParam param) {
+        Api101Bridge.init(this);
+        Api101Bridge.log("[DC] API101 module loaded process=" + param.getProcessName()
+                + " framework=" + getFrameworkName() + " api=" + getApiVersion());
+    }
+
+    @Override
+    public void onPackageReady(@NonNull PackageReadyParam param) {
+        if (!"com.miui.home".equals(param.getPackageName())) return;
+        try {
+            ClassLoader classLoader = param.getClassLoader();
+            new MainHook().handleLoadPackage(new XC_LoadPackage.LoadPackageParam(
+                    param.getPackageName(), classLoader));
+        } catch (Throwable error) {
+            Api101Bridge.log("[DC] API101 package init failed", error);
+        }
+    }
+}
