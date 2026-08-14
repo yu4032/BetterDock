3ce96bd623f86ea8f96fd19010c1f440ca789db2
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-14 15:20:47 +0800

    test: define slot based widget padding geometry

--- src/test/java/com/hellovoid/liquiddock/WidgetGridSizingTest.java
+++ src/test/java/com/hellovoid/liquiddock/WidgetGridSizingTest.java
@@ -20,57 +20,70 @@ public class WidgetGridSizingTest {
     }
 
     @Test
-    public void twoStackedTwoByOneWidgetsShareTheTwoByTwoOuterFrame() {
+    public void oneByOneUsesAlmostTheEntireCellSlot() {
         int[] xs = {0, 112, 224};
         int[] ys = {0, 108, 216};
 
-        int[] whole = WidgetGridSizing.gridRect(
-                0, 0, 2, 2, xs, ys, 100, 100, 12, 8);
-        int[] top = WidgetGridSizing.gridRect(
-                0, 0, 2, 1, xs, ys, 100, 100, 12, 8);
-        int[] bottom = WidgetGridSizing.gridRect(
-                0, 1, 2, 1, xs, ys, 100, 100, 12, 8);
+        assertArrayEquals(new int[]{4, 4, 102, 100},
+                WidgetGridSizing.slotRect(0, 0, 1, 1, xs, ys, 100, 100, 4));
+        assertArrayEquals(new int[]{110, 4, 106, 100},
+                WidgetGridSizing.slotRect(1, 0, 1, 1, xs, ys, 100, 100, 4));
+    }
 
-        assertArrayEquals(new int[]{0, 2, 212, 204}, whole);
+    @Test
+    public void everyAdjacentWidgetPairUsesTwiceTheSamePadding() {
+        int[] xs = {0, 112, 224, 336};
+        int[] ys = {0, 108, 216};
+        int padding = 4;
+
+        int[] oneByOne = WidgetGridSizing.slotRect(
+                0, 0, 1, 1, xs, ys, 100, 100, padding);
+        int[] twoByOne = WidgetGridSizing.slotRect(
+                1, 0, 2, 1, xs, ys, 100, 100, padding);
+        int[] top = WidgetGridSizing.slotRect(
+                0, 0, 2, 1, xs, ys, 100, 100, padding);
+        int[] bottom = WidgetGridSizing.slotRect(
+                0, 1, 2, 1, xs, ys, 100, 100, padding);
+
+        assertEquals(padding * 2,
+                twoByOne[0] - (oneByOne[0] + oneByOne[2]));
+        assertEquals(padding * 2,
+                bottom[1] - (top[1] + top[3]));
+    }
+
+    @Test
+    public void twoStackedTwoByOneWidgetsShareTheTwoByTwoOuterFrame() {
+        int[] xs = {0, 112, 224};
+        int[] ys = {0, 108, 216};
+        int padding = 4;
+
+        int[] whole = WidgetGridSizing.slotRect(
+                0, 0, 2, 2, xs, ys, 100, 100, padding);
+        int[] top = WidgetGridSizing.slotRect(
+                0, 0, 2, 1, xs, ys, 100, 100, padding);
+        int[] bottom = WidgetGridSizing.slotRect(
+                0, 1, 2, 1, xs, ys, 100, 100, padding);
+
         assertEquals(whole[0], top[0]);
         assertEquals(whole[0] + whole[2], top[0] + top[2]);
         assertEquals(whole[1], top[1]);
         assertEquals(whole[1] + whole[3], bottom[1] + bottom[3]);
-        assertEquals(12, bottom[1] - (top[1] + top[3]));
+        assertEquals(padding * 2,
+                bottom[1] - (top[1] + top[3]));
     }
 
     @Test
-    public void mixedOneByOneAndTwoByOneWidgetsKeepTheSameGutter() {
-        int[] xs = {0, 112, 224, 336};
-        int[] ys = {0, 108, 216};
-
-        int[] oneByOne = WidgetGridSizing.gridRect(
-                0, 0, 1, 1, xs, ys, 100, 100, 12, 8);
-        int[] twoByOne = WidgetGridSizing.gridRect(
-                1, 0, 2, 1, xs, ys, 100, 100, 12, 8);
-
-        assertEquals(12,
-                twoByOne[0] - (oneByOne[0] + oneByOne[2]));
+    public void visualPaddingScalesWithCellSize() {
+        assertEquals(4, WidgetGridSizing.visualPadding(100, 100));
+        assertEquals(8, WidgetGridSizing.visualPadding(200, 200));
+        assertEquals(1, WidgetGridSizing.visualPadding(1, 1));
     }
 
     @Test
-    public void smallerAxisReceivesOnlyTheMissingGutterInset() {
-        int[] xs = {0, 108, 216};
-        int[] ys = {0, 114, 228};
-
-        int[] left = WidgetGridSizing.gridRect(
-                0, 0, 1, 1, xs, ys, 100, 100, 8, 14);
-        int[] right = WidgetGridSizing.gridRect(
-                1, 0, 1, 1, xs, ys, 100, 100, 8, 14);
-
-        assertArrayEquals(new int[]{3, 0, 94, 100}, left);
-        assertEquals(14, right[0] - (left[0] + left[2]));
-    }
-
-    @Test
-    public void uniformGutterUsesTheLargerGridGap() {
-        assertEquals(14, WidgetGridSizing.uniformGutter(14, 8));
-        assertEquals(14, WidgetGridSizing.uniformGutter(8, 14));
-        assertEquals(0, WidgetGridSizing.uniformGutter(-2, -4));
+    public void invalidSlotGeometryReturnsEmptyRect() {
+        int[] xs = {0, 112};
+        int[] ys = {0, 108};
+        assertArrayEquals(new int[]{0, 0, 0, 0},
+                WidgetGridSizing.slotRect(1, 0, 2, 1, xs, ys, 100, 100, 4));
     }
 }
