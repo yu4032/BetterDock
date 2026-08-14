d2e69fd9bde06d20ac92aee1e1fd9c4423e7d47a
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-14 14:30:13 +0800

    test: correct uniform gutter geometry

--- src/test/java/com/hellovoid/liquiddock/WidgetGridSizingTest.java
+++ src/test/java/com/hellovoid/liquiddock/WidgetGridSizingTest.java
@@ -47,36 +47,48 @@ public class WidgetGridSizingTest {
     public void twoStackedTwoByOneWidgetsShareTheTwoByTwoOuterFrame() {
         int[] xs = {0, 112, 224};
         int[] ys = {0, 108, 216};
-        int gutter = 12;
 
         int[] whole = WidgetGridSizing.gridRect(
-                0, 0, 2, 2, xs, ys, 100, 100, gutter);
+                0, 0, 2, 2, xs, ys, 100, 100, 12, 8);
         int[] top = WidgetGridSizing.gridRect(
-                0, 0, 2, 1, xs, ys, 100, 100, gutter);
+                0, 0, 2, 1, xs, ys, 100, 100, 12, 8);
         int[] bottom = WidgetGridSizing.gridRect(
-                0, 1, 2, 1, xs, ys, 100, 100, gutter);
+                0, 1, 2, 1, xs, ys, 100, 100, 12, 8);
 
-        assertArrayEquals(new int[]{6, 6, 212, 204}, whole);
+        assertArrayEquals(new int[]{0, 2, 212, 204}, whole);
         assertEquals(whole[0], top[0]);
         assertEquals(whole[0] + whole[2], top[0] + top[2]);
         assertEquals(whole[1], top[1]);
         assertEquals(whole[1] + whole[3], bottom[1] + bottom[3]);
-        assertEquals(gutter, bottom[1] - (top[1] + top[3]));
+        assertEquals(12, bottom[1] - (top[1] + top[3]));
     }
 
     @Test
     public void mixedOneByOneAndTwoByOneWidgetsKeepTheSameGutter() {
         int[] xs = {0, 112, 224, 336};
         int[] ys = {0, 108, 216};
-        int gutter = 12;
 
         int[] oneByOne = WidgetGridSizing.gridRect(
-                0, 0, 1, 1, xs, ys, 100, 100, gutter);
+                0, 0, 1, 1, xs, ys, 100, 100, 12, 8);
         int[] twoByOne = WidgetGridSizing.gridRect(
-                1, 0, 2, 1, xs, ys, 100, 100, gutter);
+                1, 0, 2, 1, xs, ys, 100, 100, 12, 8);
 
-        assertEquals(gutter,
+        assertEquals(12,
                 twoByOne[0] - (oneByOne[0] + oneByOne[2]));
+    }
+
+    @Test
+    public void smallerAxisReceivesOnlyTheMissingGutterInset() {
+        int[] xs = {0, 108, 216};
+        int[] ys = {0, 114, 228};
+
+        int[] left = WidgetGridSizing.gridRect(
+                0, 0, 1, 1, xs, ys, 100, 100, 8, 14);
+        int[] right = WidgetGridSizing.gridRect(
+                1, 0, 1, 1, xs, ys, 100, 100, 8, 14);
+
+        assertArrayEquals(new int[]{3, 0, 94, 100}, left);
+        assertEquals(14, right[0] - (left[0] + left[2]));
     }
 
     @Test
