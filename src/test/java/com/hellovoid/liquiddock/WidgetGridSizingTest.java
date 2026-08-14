1cd996a045f9fd5c923d62945fd45ef9e6e26848
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-14 12:54:17 +0800

    test: define widget grid span sizing

---
+++ src/test/java/com/hellovoid/liquiddock/WidgetGridSizingTest.java
@@ -0,0 +1,38 @@
+package com.hellovoid.liquiddock;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WidgetGridSizingTest {
+    @Test
+    public void supportedWidgetSpecsAreLimitedToLauncherSizes() {
+        assertTrue(WidgetGridSizing.isSupportedSpec(1, 1));
+        assertTrue(WidgetGridSizing.isSupportedSpec(2, 1));
+        assertTrue(WidgetGridSizing.isSupportedSpec(2, 2));
+        assertTrue(WidgetGridSizing.isSupportedSpec(4, 2));
+
+        assertFalse(WidgetGridSizing.isSupportedSpec(3, 1));
+        assertFalse(WidgetGridSizing.isSupportedSpec(4, 1));
+    }
+
+    @Test
+    public void spanSizeIncludesEveryInternalGridGap() {
+        assertEquals(100, WidgetGridSizing.spanSize(1, 100, 12, 0, 0));
+        assertEquals(212, WidgetGridSizing.spanSize(2, 100, 12, 0, 0));
+        assertEquals(436, WidgetGridSizing.spanSize(4, 100, 12, 0, 0));
+    }
+
+    @Test
+    public void spanSizePreservesLayoutMargins() {
+        assertEquals(204, WidgetGridSizing.spanSize(2, 100, 12, 3, 5));
+        assertEquals(200, WidgetGridSizing.spanSize(2, 100, 8, 4, 4));
+    }
+
+    @Test
+    public void twoRowWidgetsIncludeTheRowGap() {
+        assertEquals(208, WidgetGridSizing.spanSize(2, 100, 8, 0, 0));
+    }
+}
