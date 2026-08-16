4669e47431be5409be92debea8b7840268ea49e4
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-17 00:16:42 +0800

    fix: install APP HOME animation hook

--- src/main/java/com/hellovoid/liquiddock/RecentsHapticHook.java
+++ src/main/java/com/hellovoid/liquiddock/RecentsHapticHook.java
@@ -11,6 +11,7 @@ final class RecentsHapticHook {
 
     static void install(ClassLoader classLoader, Listener listener) {
         RecentsExitAnimationHook.install(classLoader);
+        AppHomeAnimationHook.install(classLoader);
         String[] implementations = {
                 "com.miui.home.launcher.common.HapticFeedbackCompatLinear",
                 "com.miui.home.launcher.common.HapticFeedbackCompatV2",
