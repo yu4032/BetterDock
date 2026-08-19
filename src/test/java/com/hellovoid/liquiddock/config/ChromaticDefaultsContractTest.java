7a27110a5582e0974639695a5ce5c3a7886b909c
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-19 21:17:03 +0800

    test: require chromatic default and preset 2

---
+++ src/test/java/com/hellovoid/liquiddock/config/ChromaticDefaultsContractTest.java
@@ -0,0 +1,15 @@
+package com.hellovoid.liquiddock.config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChromaticDefaultsContractTest {
+    @Test
+    public void chromaticDefaultAndPresetAreTwo() {
+        assertEquals(Integer.valueOf(2), ConfigSchema.Glass.CHROMATIC.uiDefault());
+        assertEquals(Integer.valueOf(2), ConfigSchema.Glass.CHROMATIC.runtimeFallback());
+        assertEquals(Integer.valueOf(2), ConfigSchema.Glass.CHROMATIC.exportDefault());
+        assertEquals(Integer.valueOf(2), PresetManager.defaultValues().get("liquid_chromatic"));
+    }
+}
