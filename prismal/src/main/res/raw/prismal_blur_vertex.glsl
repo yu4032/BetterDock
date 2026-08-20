9cf7297fdc00a8fef019364ec5c347a55ea45ec2
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-20 21:09:44 +0800

    refactor: vendor official Prismal blur vertex

---
+++ prismal/src/main/res/raw/prismal_blur_vertex.glsl
@@ -0,0 +1,7 @@
+attribute vec2 a_position;
+varying vec2 v_texCoord;
+void main() {
+    gl_Position = vec4(a_position, 0.0, 1.0);
+    v_texCoord = (a_position + 1.0) * 0.5;
+    v_texCoord.y = 1.0 - v_texCoord.y;
+}
