9bcd2b7516d651736b5fa5f27cb7d4e4c5a1143c
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-21 11:08:38 +0800

    test: bind DeviceConfig count owner to module entry

--- src/test/java/com/hellovoid/liquiddock/HomeGridDeviceConfigCountContractTest.java
+++ src/test/java/com/hellovoid/liquiddock/HomeGridDeviceConfigCountContractTest.java
@@ -7,10 +7,13 @@ import java.nio.file.Path;
 import org.junit.Test;
 
 public class HomeGridDeviceConfigCountContractTest {
-    @Test public void tenBySixOverlayOwnsDeviceConfigCellCounts() throws Exception {
+    @Test public void tenBySixOwnsDeviceConfigCellCounts() throws Exception {
+        String entry = Files.readString(Path.of(
+                "src/main/java/com/hellovoid/liquiddock/ModuleMain.java"));
         String source = Files.readString(Path.of(
-                "src/main/java/com/hellovoid/liquiddock/HomeGridProfileOverlayHook.java"));
+                "src/main/java/com/hellovoid/liquiddock/HomeGridDeviceConfigCountHook.java"));
 
+        assertTrue(entry.contains("HomeGridDeviceConfigCountHook.install(classLoader"));
         assertTrue(source.contains("com.miui.home.launcher.DeviceConfig"));
         assertTrue(source.contains("getCellCountX"));
         assertTrue(source.contains("getCellCountY"));
