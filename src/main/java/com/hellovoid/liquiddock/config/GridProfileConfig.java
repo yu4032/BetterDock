00097891d1c6589747a5cf0317c3cc983d73e8ce
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-21 22:11:19 +0800

    refactor: reduce grid profile config to persisted selection

--- src/main/java/com/hellovoid/liquiddock/config/GridProfileConfig.java
+++ src/main/java/com/hellovoid/liquiddock/config/GridProfileConfig.java
@@ -1,12 +1,8 @@
 package com.hellovoid.liquiddock.config;
 
-/** Canonical persisted keys for optional extended home-screen layouts. */
+/** Persisted selection for the supported home-screen grid profiles. */
 public final class GridProfileConfig {
-    public static final String ENABLED_KEY = "home_grid_extended";
     public static final String PROFILE_KEY = "grid_profile";
-    public static final String LEGACY_8X4_KEY = "home_grid_8x4";
-
-    public static final boolean DEFAULT_ENABLED = false;
     public static final String DEFAULT_PROFILE = "8x4";
 
     private GridProfileConfig() {}
