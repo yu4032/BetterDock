67aff38522c6b21d4346b9ccb057fc3ebae059b5
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-21 22:10:24 +0800

    cleanup: remove experimental wording from profile test

--- src/test/java/com/hellovoid/liquiddock/config/GridProfileConfigTest.java
+++ src/test/java/com/hellovoid/liquiddock/config/GridProfileConfigTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GridProfileConfigTest {
-    @Test public void normalizesOnlySupportedExperimentalProfile() {
+    @Test public void normalizesOnlySupportedProfiles() {
         assertEquals("10x6", GridProfileConfig.normalizeProfile("10x6"));
         assertEquals("10x6", GridProfileConfig.normalizeProfile("10X6"));
         assertEquals("8x4", GridProfileConfig.normalizeProfile("8x4"));
