fc26001d3b376f136a19728f96529de7f3cbb5ca
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-21 10:15:53 +0800

    fix: install 10x6 horizontal centering correction

--- src/main/java/com/hellovoid/liquiddock/ModuleMain.java
+++ src/main/java/com/hellovoid/liquiddock/ModuleMain.java
@@ -41,6 +41,8 @@ public final class ModuleMain extends XposedModule {
             new MainHook().install(classLoader);
             HomeGridProfileOverlayHook.install(classLoader,
                     customGridEnabled, selectedProfile);
+            HomeGridHorizontalCenteringHook.install(classLoader,
+                    customGridEnabled, selectedProfile);
             HomeGridRotationBridge.install(classLoader,
                     customGridEnabled, selectedProfile);
             WorkspaceDropRuleHook.install(classLoader, customGridEnabled);
