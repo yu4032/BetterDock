49f55c76f935e0aecbb0ee2408904353e940a1f9
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-23 01:38:28 +0800

    test: include folder radius in config export defaults

--- src/test/java/com/hellovoid/liquiddock/config/ConfigCodecTest.java
+++ src/test/java/com/hellovoid/liquiddock/config/ConfigCodecTest.java
@@ -196,7 +196,7 @@ public class ConfigCodecTest {
     public void absentPreferencesExportCompleteHistoricalDefaults() {
         Map<String, Object> exported = ConfigCodec.exportValues(new HashMap<>());
 
-        assertEquals(129, exported.size());
+        assertEquals(130, exported.size());
         assertEquals(Boolean.TRUE, exported.get("liquiddock_enabled"));
         assertEquals(Boolean.FALSE, exported.get("home_grid_8x4"));
         assertEquals(Boolean.FALSE, exported.get("grid_widget_adaptation"));
@@ -207,6 +207,7 @@ public class ConfigCodecTest {
         assertEquals(42, exported.get("dock_shadow_radius"));
         assertEquals(Boolean.FALSE, exported.get("liquid_glass"));
         assertEquals(Boolean.TRUE, exported.get("liquid_folder_glass"));
+        assertEquals(0, exported.get("liquid_folder_corner_radius"));
         assertEquals(Boolean.TRUE, exported.get("liquid_dimensions_dp"));
         assertFalse(exported.containsKey("liquid_legacy_s_curve"));
         assertEquals(0, exported.get("liquid_sampling_extra_top"));
